fc2a421b3a18bfb11b25083f2cfbb164e376e7e9
Asogwa Emmanuel <asogwaemmanuel36@gmail.com>
2024-02-22 14:43:44 +0530

    added pid killing

--- core/main_process.py
+++ core/main_process.py
@@ -1,18 +1,27 @@
 import websocket
+import threading
 import warnings
 import time
+import os
 
+from  platform_dependent import find_apsV3
 import mqtt_client
 import ws_client
 import setup
-from  platform_dependent import find_apsV3
-
 
 # Suppress all warnings
 warnings.filterwarnings("ignore")
 
 
+
 if __name__ == "__main__":
+    global mqtt_client_conn
+
+    # ctrl + c does not kill ws.run_forever(). 
+    # use the PID to kill it instead
+    # on powershell:
+    #taskkill /F /PID <PID>
+    print(f"Process ID (PID): {os.getpid()}")
 
     user_data = setup.setup()
 
@@ -24,8 +33,19 @@ if __name__ == "__main__":
 
     #print(user_data)
 
-    mqtt_client = mqtt_client.start_mqtt_client(user_data["username"], user_data["mqtt_endpoint"])
-    #ws_client.start_ws_client(user_data["server_endpoint"])
+    # start mqtt client
+    server_endpoint = user_data["mqtt_endpoint"]
+    username = user_data["username"]
+    ws_client.topic = username
+    ws_client.mqtt_client_conn = mqtt_client.start_mqtt_client(username, server_endpoint)
+
+    # start ws client
+    server_endpoint = user_data["server_endpoint"]
+    wss_client_conn = ws_client.start_ws_client(server_endpoint)
+
+    # run ws client indefinetly in another thread
+    thread = threading.Thread( target = ws_client.run_ws_indefinte, args = (wss_client_conn,))
+    thread.start()
 
     while True:
             
@@ -40,11 +60,13 @@ if __name__ == "__main__":
             # Check if it's not the last row
             if index < len(find_apsV3.detected_aps) - 1:
                 fingerprint += "\n"
-
+        
+        # format fingerprint appropirately
+        fingerprint = fingerprint.replace(':', '')
         print(fingerprint,"\n\n")
 
         #send scan results to server 
-        #ws.send(fingerprint)
+        wss_client_conn.send(fingerprint)
 
         time.sleep(10) 
 
